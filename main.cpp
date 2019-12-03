b651e978250e440d55603ec8d09cf793eee9f86f
sarahorsak <45637628+sarahorsak@users.noreply.github.com>
2019-12-03 16:19:56 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <queue>
 #include <fstream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -15,15 +16,20 @@ struct hashNode{
     hashNode *nextCollision;
 };
 
+struct collision{
+    int filename1;
+    int filename2;
+    int num_collisions;
+};
+
 string makeString(queue<string> words);
 int hashFunc(hashNode node, int tableSize);
 void insertHash(hashNode *hashTable[], int tableIndex, hashNode &node);
+bool compareCollision(collision *c1, collision *c2);
+int getdir (string dir, vector<string> &files);
 
 
-
-/*function... might want it in some class?*/
-int getdir (string dir, vector<string> &files)
-{
+int getdir (string dir, vector<string> &files){
     DIR *dp;
     struct dirent *dirp;
     if((dp  = opendir(dir.c_str())) == NULL) {
@@ -73,6 +79,7 @@ int main(int argc, char *argv[])
          //open first file
          ifstream myfile;
          myfile.open(files[i].c_str());
+         int fileIndex = i;
 
          //create queue
          queue<string> words = queue<string>();
@@ -86,12 +93,14 @@ int main(int argc, char *argv[])
          }
 
          //then move the index of file name to hash table
-         //get hash key based off the chunk of words
+
          string chunk = makeString(words);
          hashNode *current = new hashNode;
          current->chunk = chunk;
-         current->filename = i;
+         current->filename = fileIndex;
          current->nextCollision = NULL;
+
+         //get hash key based off the chunk of words
          int tableIndex = hashFunc(*current, tableSize);
          insertHash(hashTable, tableIndex, *current);
 
@@ -102,12 +111,13 @@ int main(int argc, char *argv[])
             words.pop();
             words.push(word);
             //move to hash
-            //get hash key based off chunk
             string chunk = makeString(words);
             hashNode *current = new hashNode;
             current->chunk = chunk;
-            current->filename = i;
+            current->filename = fileIndex;
             current->nextCollision = NULL;
+
+            //get hash key based off chunk
             int tableIndex = hashFunc(*current, tableSize);
             insertHash(hashTable, tableIndex, *current);
         }
@@ -116,7 +126,58 @@ int main(int argc, char *argv[])
     }
 
 //need to go through hash table and get collisions into a 2D array
-//compare if collisions are > inputted collision number then print the file names
+//traverse through hash table
+    for (int i = 0; i < tableSize; i ++){
+        //if there's a node at that location
+        hashNode *first = hashTable[i];
+        //while the first in the list isn't null
+        while (first!= NULL){
+            //set current = to first->next
+            hashNode *node = first;
+            hashNode *current = first->nextCollision;
+            //while current isn't null, then add collision to the grid
+            while (current != NULL){
+                //if they are diff files, add collision
+                if (first ->filename != current->filename){
+                    grid[first->filename][current->filename]++;
+                }
+                //alter current to next one in linked list
+                current = current-> nextCollision;
+                //add all the collisions for first in linked list
+            }
+            //alter first to be the next one and repeat
+            first = first->nextCollision;
+            //delete the one just accessed
+            delete node;
+        }
+    }
+    //traverse through the grid and create a list of collisions
+    vector<collision*> collisions;
+    for (int r = 0; r < files.size(); r++){
+        for (int c = 0; c < files.size(); c++){
+            if (grid[r][c] >= num_similar){
+                collision *temp = new collision;
+                temp->filename1= r;
+                temp->filename2 = c;
+                temp->num_collisions = grid[r][c];
+                collisions.push_back(temp);
+            }
+        }
+    }
+
+    //sort collision data by using built in sort
+    sort(collisions.begin(), collisions.end(), compareCollision);
+
+    //print collision data
+    for (int i = 0; i < collisions.size(); i++){
+        char file1 = (char)(collisions[i]->filename1 + 65);
+        char file2 = (char)(collisions[i]->filename2 + 65);
+
+        cout << collisions[i]->num_collisions << ": "
+        << "filename" << file1 << ", "
+        << "filename" << file2 <<endl;
+    }
+
 }
 
 //makes the words from queue into a string chunk
@@ -124,7 +185,17 @@ string makeString(queue<string> words){
     string result = "";
     while (!words.empty()){
         string word = words.front();
-        result += word;
+        //iterate by letter
+        for (int letter = 0; letter < word.length(); letter++){
+            //if caps switch to lowercase
+            if (word[letter]>= 'A' && word[letter]<= 'Z'){
+                word[letter] = word[letter]+32;
+            }
+            //if valid letter then add to string
+            if (word[letter]>= 'a' && word[letter]<= 'z'){
+                result += word[letter];
+            }
+        }
         words.pop();
     }
     return result;
@@ -140,10 +211,14 @@ void insertHash(hashNode *hashTable[], int tableIndex, hashNode &node){
     if (hashTable[tableIndex] == NULL) {                        //if empty, add file node
         hashTable[tableIndex] = &node;
     }
-    else {                                                      //if not empty, add to head of linked list
+    else {                                                                      //if not empty, add to head of linked list
         hashNode *temp = hashTable[tableIndex];
         hashTable[tableIndex] = &node;
         node.nextCollision = temp;
     }
 
+}
+
+bool compareCollision(collision *c1, collision *c2){
+    return (c1->num_collisions > c2->num_collisions);
 }
